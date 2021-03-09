e1529f95ac7d03d32e41e0f8be3319bdbb59be94
usman-naeem728 <76055167+usman-naeem728@users.noreply.github.com>
2021-03-08 22:10:00 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function openmenu(){
+    document.getElementById("side-menu").style.display="block"
+    document.getElementById("menu-btn").style.display="none"
+    document.getElementById("menu-close-btn").style.display="block"
+
+}
+function closemenu(){
+    document.getElementById("side-menu").style.display="none"
+    document.getElementById("menu-btn").style.display="block"
+    document.getElementById("menu-close-btn").style.display="none"
+
+}
